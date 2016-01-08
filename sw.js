c8a5e58a652ecd4b64e5549f551c272594b281ef
Hitesh Aleriya <hitesh.aleriya@naukri.com>
2016-01-08 11:19:47 +0530

    removed onload

--- sw.js
+++ sw.js
@@ -1,17 +1,15 @@
-window.addEventListener('load', function() {
-	if ('serviceWorker' in navigator) {
-	  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-	    // Registration was successful
-	    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-	  }).catch(function(err) {
-	    // registration failed :(
-	    console.log('ServiceWorker registration failed: ', err);
-	  });
-	}
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
 
-	window.addEventListener('beforeinstallprompt', function(e) {
-  		console.log('beforeinstallprompt Event fired');
-  		e.preventDefault();
-  		return false;
-    });
+window.addEventListener('beforeinstallprompt', function(e) {
+	console.log('beforeinstallprompt Event fired');
+	e.preventDefault();
+	return false;
 });
