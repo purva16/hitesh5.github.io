3be667cc82a741494e52e04e6265246aaa83d4a1
Hitesh Aleriya <hitesh.aleriya@naukri.com>
2016-01-08 11:14:46 +0530

    cancel default event.

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
+window.addEventListener('load', function() {
+	if ('serviceWorker' in navigator) {
+	  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+	    // Registration was successful
+	    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+	  }).catch(function(err) {
+	    // registration failed :(
+	    console.log('ServiceWorker registration failed: ', err);
+	  });
+	}
+
+	window.addEventListener('beforeinstallprompt', function(e) {
+  		console.log('beforeinstallprompt Event fired');
+  		e.preventDefault();
+  		return false;
+    });
+});
