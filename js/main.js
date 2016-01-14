94c02432e73b1bde5dc9d7b9db669def38971b4a
Hitesh Aleriya <hitesh.aleriya@naukri.com>
2016-01-14 10:53:59 +0530

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 if ('serviceWorker' in navigator) {
 	console.log('Service Worker is supported');
- 	navigator.serviceWorker.register('/sw.js').then(function(reg) {
+ 	navigator.serviceWorker.register('sw.js').then(function(reg) {
    		console.log(':^)', reg);
    		reg.pushManager.subscribe({
             userVisibleOnly: true
